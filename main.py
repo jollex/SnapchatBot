b8fa88884af4d999f5c8a85d79e948e86b36ff09
Alex Jolly <jolly_alex@yahoo.com>
2015-01-15 01:18:47 -0500

    Cleaned up

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 
 from pysnap import Snapchat
 import secrets
+import os
+
+path = os.path.dirname(os.path.realpath(__file__)) + '/tmp.jpg'
 
 s = Snapchat()
 s.login(secrets.USERNAME, secrets.PASSWORD)
 
-friends_to_add = [friend['name'] for friend in s.get_updates()['added_friends'] if friend['type'] == 1]
-for friend in friends_to_add:
+for friend in [friend['name'] for friend in s.get_updates()['added_friends'] if friend['type'] == 1]:
     s.add_friend(friend)
 
-snaps = [snap['id'] for snap in s.get_snaps() if snap['status'] == 1 and snap['media_type'] == 0]
+snaps = [snap for snap in s.get_snaps() if snap['status'] == 1 and snap['media_type'] == 0]
 for snap in snaps:
-    with open('~/SnapchatBot/tmp.jpg', 'wb') as f:
-        f.write(s.get_blob(snap))
-    media_id = s.upload('~/SnapchatBot/tmp.jpg')
-    s.post_story(media_id, 5)
-    s.mark_viewed(snap)
+    with open(path, 'wb') as f:
+        f.write(s.get_blob(snap['id']))
+    media_id = s.upload(path)
+    print s.post_story(media_id, 5)
+    s.mark_viewed(snap['id'])
