9a51358871f04e2a5552621b6ac2c9dbe1ee8345
Alex Jolly <jolly_alex@yahoo.com>
2015-01-14 21:15:28 -0500

    Save temporary pictures to local directory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 
 from pysnap import Snapchat
 import secrets
@@ -12,8 +12,8 @@ for friend in friends_to_add:
 
 snaps = [snap['id'] for snap in s.get_snaps() if snap['status'] == 1 and snap['media_type'] == 0]
 for snap in snaps:
-    with open('tmp.jpg', 'wb') as f:
+    with open('~/SnapchatBot/tmp.jpg', 'wb') as f:
         f.write(s.get_blob(snap))
-    media_id = s.upload('tmp.jpg')
+    media_id = s.upload('~/SnapchatBot/tmp.jpg')
     s.post_story(media_id, 5)
     s.mark_viewed(snap)
